dd6b449920c8508845e65fc21c1b1ec2361928f0
Matheus <matheusfers3500@gmail.com>
2018-08-21 14:14:06 -0300

    Started module compilation

--- PyAFM/PyAFM.cpp
+++ PyAFM/PyAFM.cpp
@@ -2,65 +2,102 @@
 #include <fstream>
 #include <math.h>
 
-#define POINTS 2048 // Number of points to be ploted
+#include <boost/python.hpp>
+#include <boost/python/numpy.hpp>
+#include <boost/python/def.hpp>
+#include <boost/python/module.hpp>
+
+using namespace boost::python;
+namespace np = boost::python::numpy;
 
 
-void getAMWave(float carrier_frequency, float am_frequency, float amplitude, float depth,float* wave, float dt)
+class AFMWave
 {
-    /// This function creates an AM wave according to the parameters
-    /// and stores it into the wave vector
-
-    float s; // wave value in a given time
+public:
     float pi = M_PI;
 
-    for(int i = 0; i < POINTS; i++) // calculates the AM wave's value and store it into wave
+    AFMWave(float carrier_frequency, float amplitude, int buffer_size)
     {
-        s = amplitude * (1 + depth * sin(2 * pi * am_frequency * dt * i));
-        float aux = 2 * pi * carrier_frequency * dt * i;
-        s = s * sin(aux);
-        aux = 1 + pow(depth, 2) / 2;
-        aux = sqrt(aux);
-        s /= aux;
-        wave[i] = s;
+        this->carrier_frequency = carrier_frequency;
+        this->amplitude = amplitude;
+        this->buffer_size = buffer_size;
     }
-}
-void getFMWave(float carrier_frequency, float fm_frequency, float amplitude, float depth, float* wave, float dt)
+    /// Setter methods
+    void setCarrierFrequency(float frequency) {this->carrier_frequency = frequency;}
+    void setAmplitude(float amplitude) {this->amplitude = amplitude;}
+    void setAMFrequency(float frequency) {this->am_frequency = frequency;}
+    void setFMFrequency(float frequency) {this->fm_frequency = frequency;}
+    void setAMDepth(float depth) {this->am_depth = depth;}
+    void setFMDepth(float depth) {this->fm_depth = depth;}
+    void setDT(float dt) {this->dt = dt;}
+    void setBufferSize(int buffer_size) {this->buffer_size = buffer_size;}
+    /// Getter methods
+    float getCarrierFrequency() {return this->carrier_frequency;}
+    float getAmplitude() {return this->amplitude;}
+    float getAMFrequency() {return this->am_frequency;}
+    float getFMFrequency() {return this->fm_frequency;}
+    float getAMDepth() {return this->am_depth;}
+    float getFMDepth() {return this->fm_depth;}
+    float getDT() {return this->dt;}
+    int getBufferSize() {return this->buffer_size;}
+    /// Utilities methods
+    np::ndarray getAMWave()
+    {
+        /// This method creates an AM wave according to the parameters
+        /// and stores it into the wave numpy ndarray
+
+//        boost::python::tuple shape = boost::python::make_tuple(buffer_size, 1);
+//        np::dtype dtype = np::dtype::get_builtin<float>();
+
+        np::array wave = np::zeros(shape, dtype);
+        float s;
+
+        for(int i = 0; i < buffer_size; i++) // calculates the AM wave's value and store it into wave
+        {
+            s = amplitude * (1 + am_depth * sin(2 * pi * am_frequency * dt * i));
+            float aux = 2 * pi * carrier_frequency * dt * i;
+            s = s * sin(aux);
+            aux = 1 + pow(am_depth, 2) / 2;
+            aux = sqrt(aux);
+            s /= aux;
+            wave[i] = s;
+        }
+        return wave;
+    }
+
+private:
+    float carrier_frequency;
+    float amplitude;
+    float am_frequency;
+    float fm_frequency;
+    float am_depth;
+    float fm_depth;
+    float dt;
+    int buffer_size;
+};
+
+/// Python wraper
+BOOST_PYTHON_MODULE(PyAFM)
 {
-    /// This function creates an FM wave according to the parameters
-    /// and stores it into the wave vector
+    Py_Initialize();
+    np::initialize();
 
-    float s; // wave value in a given time
-    float pi = M_PI;
-
-    for(int i = 0; i < POINTS; i++) // calculates the FM wave's value and store it into wave
-    {
-        s = 2 * pi * carrier_frequency * dt * i;
-        s += ((depth * fm_frequency) / (2 * fm_frequency) * sin(2 * pi * fm_frequency * dt * i));
-        s = sin(s);
-        s = amplitude * s;
-        wave[i] = s;
-    }
-}
-
-int main()
-{
-    /// This function will be erased in a near future,
-    /// for now, it is used to test the AM and FM functions output
-
-    std::ofstream file1;
-    std::ofstream file2;
-    file1.open("am.dat"); // create a am.dat file if its not created yet
-    file2.open("fm.dat"); // create a fm.dat file if its not created yet
-    float wave[POINTS], dt = 0.0001; // initialize the wave's values buffer and set delta time
-    getAMWave(60, 10, 2, .5, wave, dt); // call getAMWave to test the output
-    for(int i = 0; i < POINTS; i++) // write the wave buffer to file1
-    {
-        file1 << i * dt << "    " << wave[i] << std::endl;
-    }
-    getFMWave(100, 40, 2, .75, wave, dt); // call getFMWave to test the output
-    for(int i = 0; i < POINTS; i++) // write the wave buffer to file2
-    {
-        file2 << i * dt << "    " << wave[i] << std::endl;
-    }
-    return 0;
+    class_<AFMWave>("AFMWave", init<float, float, int>())
+        .def("setCarrierFrequency", &AFMWave::setCarrierFrequency, arg("frequency"))
+        .def("setAmplitude", &AFMWave::setAmplitude, arg("amplitude"))
+        .def("setAMFrequency", &AFMWave::setAMFrequency, arg("frequency"))
+        .def("setFMFrequency", &AFMWave::setFMFrequency, arg("frequency"))
+        .def("setAMDepth", &AFMWave::setAMDepth, arg("depth"))
+        .def("setFMDepth", &AFMWave::setFMDepth, arg("depth"))
+        .def("setDT", &AFMWave::setDT, arg("dt"))
+        .def("setBufferSize", &AFMWave::setBufferSize, arg("buffer_size"))
+        .def("getCarrierFrequency", &AFMWave::getCarrierFrequency)
+        .def("getAmplitude", &AFMWave::getAmplitude)
+        .def("getAMFrequency", &AFMWave::getAMFrequency)
+        .def("getFMFrequency", &AFMWave::getFMFrequency)
+        .def("getAMDepth", &AFMWave::getAMDepth)
+        .def("getFMDepth", &AFMWave::getFMDepth)
+        .def("getDT", &AFMWave::getDT)
+        .def("getBufferSize", &AFMWave::getBufferSize)
+        .def("getAMWave", &AFMWave::getAMWave);
 }
